ea4ffdf70dae5be57204d221bea30c53cbe0c409
thiago <thiagoprocaci@gmail.com>
2018-06-18 20:47:56 -0300

    reorganizing files

--- scripts/simplewalk/gen.sh
+++ scripts/simplewalk/gen.sh
@@ -9,10 +9,11 @@ FEATURE_LEARNING_NODE_2_VEC="/home/thiago/Área de Trabalho/tbp/workspace/node2v
 FEATURE_LEARNING_LINE_DIR="/home/thiago/Área de Trabalho/tbp/workspace/LINE/linux/"
 EDGE_LIST_BIOLOGY="/home/thiago/Área de Trabalho/tbp/workspace/node2vec/web-sci/biology.edgelist.csv"
 EDGE_LIST_CHEMISTRY="/home/thiago/Área de Trabalho/tbp/workspace/node2vec/web-sci/chemistry.edgelist.csv"
-DIM=15
-OUTPUT_DIR="/home/thiago/Área de Trabalho/tbp/workspace/node2vec/scripts"
-BIOLOGY_CLASS_FILE="../web-sci/biology.class.csv"
-CHEMISTRY_CLASS_FILE="../web-sci/chemistry.class.csv"
+DIM=20
+OUTPUT_DIR="/home/thiago/Área de Trabalho/tbp/workspace/node2vec/scripts/"
+BIOLOGY_CLASS_FILE="../../web-sci/biology.class.csv"
+CHEMISTRY_CLASS_FILE="../../web-sci/chemistry.class.csv"
+
 
 
 COMMUNITY="biology"
@@ -59,7 +60,7 @@ cd "$CURR_DIR"
 python2 "$FEATURE_LEARNING_NODE_2_VEC"  --input="$EDGE_LIST_BIOLOGY" --output="$OUTPUT_NODE_2_VEC_Q_GRT_P_EMD" --dimensions="$DIM" --q=1.5 --p=0.5
 python2 "$FEATURE_LEARNING_NODE_2_VEC"  --input="$EDGE_LIST_BIOLOGY" --output="$OUTPUT_NODE_2_VEC_Q_LST_P_EMD" --dimensions="$DIM" --q=0.5 --p=1.5
 python2 "$FEATURE_LEARNING_NODE_2_VEC"  --input="$EDGE_LIST_BIOLOGY" --output="$OUTPUT_NODE_2_VEC_Q_EQ_P_EMD" --dimensions="$DIM" --q=1 --p=1
-python2 "$FEATURE_LEARNING_NODE_2_VEC"  --input="$EDGE_LIST_BIOLOGY" --output="$OUTPUT_BFS_EMD" --dimensions="$DIM" --executionType=smt
+python2 "$FEATURE_LEARNING_NODE_2_VEC"  --input="$EDGE_LIST_BIOLOGY" --output="$OUTPUT_BFS_EMD" --dimensions="$DIM" --executionType=smt --num-walks=1
 
 
 sed -i '1d' "$OUTPUT_NODE_2_VEC_Q_GRT_P_EMD"
@@ -124,7 +125,7 @@ cd "$CURR_DIR"
 python2 "$FEATURE_LEARNING_NODE_2_VEC"  --input="$EDGE_LIST_CHEMISTRY" --output="$OUTPUT_NODE_2_VEC_Q_GRT_P_EMD" --dimensions="$DIM" --q=1.5 --p=0.5
 python2 "$FEATURE_LEARNING_NODE_2_VEC"  --input="$EDGE_LIST_CHEMISTRY" --output="$OUTPUT_NODE_2_VEC_Q_LST_P_EMD" --dimensions="$DIM" --q=0.5 --p=1.5
 python2 "$FEATURE_LEARNING_NODE_2_VEC"  --input="$EDGE_LIST_CHEMISTRY" --output="$OUTPUT_NODE_2_VEC_Q_EQ_P_EMD" --dimensions="$DIM" --q=1 --p=1
-python2 "$FEATURE_LEARNING_NODE_2_VEC"  --input="$EDGE_LIST_CHEMISTRY" --output="$OUTPUT_BFS_EMD" --dimensions="$DIM" --executionType=smt
+python2 "$FEATURE_LEARNING_NODE_2_VEC"  --input="$EDGE_LIST_CHEMISTRY" --output="$OUTPUT_BFS_EMD" --dimensions="$DIM" --executionType=smt --num-walks=1
 
 
 sed -i '1d' "$OUTPUT_NODE_2_VEC_Q_GRT_P_EMD"
@@ -144,3 +145,6 @@ Rscript biology-ml.R "$OUTPUT_LINE_1_AUC" "$OUTPUT_LINE_1_EMD" "$CHEMISTRY_CLASS
 
 
 # python2 src/main.py  --input=web-sci/chemistry.edgelist.csv --output=web-sci/chemistry-directed-node2vec-q1-p1.emd --dimensions=5 --q=1 --p=1
+
+mv ../*.auc .
+mv ../*.emd .
---
+++ scripts/summary/Summary.py
@@ -0,0 +1,88 @@
+from os import listdir
+from os.path import isfile, join
+import argparse
+
+
+class Result:
+	
+	def __init__(self, fileName, auc):		
+		if "directed-bfs" in fileName:
+			self.method = "simpleWalk"
+		if "q0.5-p1.5" in fileName:
+			self.method = "node2Vec DFS-like"
+		if "q1.5-p0.5" in fileName:
+			self.method = "node2Vec BFS-like"
+		if "q1-p1" in fileName:
+			self.method = "DeepWalk"
+		if "line-order-1" in fileName:
+			self.method = "LINE First Order"
+		if "line-order-2" in fileName:
+			self.method = "LINE Second Order"
+		array = fileName.split(".")
+		self.dim = int(array[len(array) - 2])
+		array = fileName.split("/")
+		self.fileName = array[len(array) - 1]
+		self.auc = auc.replace('\n', '')
+		array = self.fileName.split("-")
+		self.community = array[0]
+
+def export2Html(resultList):
+	communityList = getCommunities(resultList)
+	dimensionList = getDimensions(resultList)
+	sortedList = sorted(resultList, key=lambda r: r.dim)
+
+	tdTemplate = "<tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td></tr>"
+	html = "<html><body><h2>Feature Learning AUC</h2><table  border='1'>"
+	html = html + "<tr><th>Community</th><th>Method</th><th>Dimension</th><th>AUC</th><th>File</th></tr>"
+	for community in communityList:
+		for dim in dimensionList:
+			sortedList = [x for x in resultList if x.dim == dim and x.community == community]
+			sortedList = sorted(sortedList, key=lambda r: r.fileName)
+			for r in sortedList:
+				html = html + tdTemplate.format(r.community, r.method, r.dim, r.auc, r.fileName)
+	html = html + "</table></body></html>"
+	return html
+
+def getCommunities(resultList):
+	communityList = []
+	for r in resultList:
+		if r.community not in communityList:
+			communityList.append(r.community)
+	return sorted(communityList)
+
+def getDimensions(resultList):
+	dimList = []
+	for r in resultList:
+		if r.dim not in dimList:
+			dimList.append(r.dim)
+	return sorted(dimList)
+
+
+def parse_args():
+	parser = argparse.ArgumentParser(description="Run Summary")
+	parser.add_argument('--folder', dest='folder', type=str, default="")
+	return parser.parse_args()
+
+
+def main(args):
+	aucfileList = [join(args.folder, f) for f in listdir(args.folder) if isfile(join(args.folder, f)) and isAUC(join(args.folder, f))]
+	resultList = []
+	for file in aucfileList:
+		with open(file, 'r') as aucFile:
+			auc = aucFile.read()
+			r = Result(file, auc)
+			resultList.append(r)
+	html = export2Html(resultList)
+	print(html)
+	#for r in resultList:
+	#	print r.fileName + " " + r.method + " " + r.dim + " " + r.auc + " " + r.community
+	#print(len(aucfiles))
+
+def isAUC(filePath):
+	if filePath is not None and filePath.endswith(".auc"):
+		return True
+	return False
+
+if __name__ == "__main__":
+	args = parse_args()
+	main(args)
