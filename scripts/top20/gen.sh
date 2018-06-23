9a907c113dc1a31b6e0cea674c831e8d1cd6a964
thiago <thiagoprocaci@gmail.com>
2018-06-23 20:02:55 -0300

    adding results

--- scripts/top20/gen.sh
+++ scripts/top20/gen.sh
@@ -5,7 +5,7 @@ CURR_DIR=$(pwd)
 
 #echo "$CURR_DIR"
 
-DIM=25
+DIM=30
 OUTPUT_DIR="/home/thiago/Área de Trabalho/tbp/workspace/node2vec/scripts/top20"
 BIOLOGY_CLASS_FILE="biology.class.csv"
 CHEMISTRY_CLASS_FILE="chemistry.class.csv"
